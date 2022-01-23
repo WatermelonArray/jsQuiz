40532ad8d0ab1360dd9f8a06fb6c54796a718ca8
Lucas Himschoot <34957838+WatermelonArray@users.noreply.github.com>
2022-01-23 11:25:08 +0000

    Added basic classes for questions

---
+++ static/js/quizClasses.js
@@ -0,0 +1,14 @@
+"use static"
+
+// setup classes
+
+let questionAnswer = {
+	description: "A",
+	isAnswer: true
+}
+let questionTemplate = {
+	question: "Hello World",
+	answers: []
+}
+
+export {quizAnswer, quizTemplate}
---
+++ static/js/quizgame.js
@@ -0,0 +1,10 @@
+"use static"
+
+// for future reference: https://softwareengineering.stackexchange.com/questions/119181/what-type-of-encoding-can-i-use-to-make-a-string-shorter
+
+// setup classes
+import {questionAnswer, questionTemplate} from "quizClasses.js";
+
+currentQuestion = new quizTemplate;
+
+
