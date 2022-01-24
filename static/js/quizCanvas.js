44f6e6f4e7a38ec273ff71fe0832aa6b607c1431
Lucas Himschoot <34957838+WatermelonArray@users.noreply.github.com>
2022-01-24 22:43:39 +0000

    Added animations to the title screen

--- static/js/quizCanvas.js
+++ static/js/quizCanvas.js
@@ -2,21 +2,15 @@
 
 
 import {udim, udim2} from "./api/udim.js"
-import {menuText, startText} from "./src/menu/textRender.js";
+import {menuBackground, menuBar, menuText, startText} from "./src/menu.js";			// main menu animation
 
 let canvas = document.getElementById("gameCanvas");
 let context = canvas.getContext("2d");
-
-let hslColor = [0, 20, 50];
 
 function getMouseCords() {
 
 }
 function isMouseInsideCords() {
-
-}
-
-function playIntro() {
 
 }
 
@@ -26,26 +20,10 @@ function draw() {
 	canvas.width = window.innerWidth;
 	canvas.height = window.innerHeight;
 
-	// set current frame variables
-	const cw = canvas.width;
-	const ch = canvas.height;
-	
-	hslColor[0] = hslColor[0] + 0.5;
-	if (hslColor[0] > 359) {
-		hslColor[0] = 0;
-	}
-
 	// create background
-	context.clearRect(0, 0, cw, ch);
-	context.fillStyle = "hsl(" + hslColor[0] + "," + hslColor[1] + "%," + hslColor[2] + "%)";
-	let blackBar = context.fillRect(0, 0, cw, ch);
-
-	context.fillStyle = "#000000"
-	context.globalAlpha = 0.75;
-	context.fillRect(0, (ch/2) - (64*1.5), cw, ch/6);
-
+	menuBackground(canvas, context);
+	menuBar(canvas, context);
 	menuText(canvas, context);
-
 	startText(canvas, context);
 
 	// solution found for looping correctly:
---
+++ static/js/src/menu.js
@@ -0,0 +1,95 @@
+"use strict";
+
+let hslColor = [0, 20, 50];
+function menuBackground(canvas, context) {
+
+	const cw = canvas.width;
+	const ch = canvas.height;
+
+	hslColor[0] = hslColor[0] + 0.5;
+
+	if (hslColor[0] > 359) {hslColor[0] = 0;}
+
+	context.clearRect(0, 0, cw, ch);
+	context.fillStyle = "hsl(" + hslColor[0] + "," + hslColor[1] + "%," + hslColor[2] + "%)";
+	context.fillRect(0, 0, cw, ch);
+
+}
+
+let barSize = -60		// negative means frames AFTER start of renderer
+let c_barSize = 0
+function menuBar(canvas, context) {
+
+	const cw = canvas.width;
+	const ch = canvas.height;
+
+	if (barSize < 8 && barSize > 0) {
+		barSize++
+		c_barSize = barSize
+	}
+	if (barSize <= 0) {
+		barSize++
+	}
+
+	context.fillStyle = "#000000"
+	context.globalAlpha = 0.75;
+	context.fillRect(
+		(cw / 2) - ((cw / 16) * c_barSize),
+		(ch / 2) - 100,
+		(cw / 8) * c_barSize,
+		200
+	);
+
+}
+
+let textLoc = -90
+let c_textLoc = 0
+function menuText(canvas, context) {
+
+	const cw = canvas.width;
+	const ch = canvas.height;
+
+	if (textLoc < 16 && textLoc > 0) {
+		textLoc++;
+		c_textLoc = textLoc;
+	}
+	if (textLoc <= 0) {
+		textLoc++;
+	}
+
+	// menu title
+	context.globalAlpha = 1;
+	context.font = "64px Helvetica";
+	context.fillStyle = "#FFFFFF";
+	context.textAlign = "center";
+	context.fillText("JavaScript Quiz Game",
+		(cw / 2) - ((cw / 2) / c_textLoc),		// cool tween effect
+		ch / 2 + 16
+	);
+
+
+}
+
+let flash = 1
+function startText(canvas, context) {
+
+	const cw = canvas.width;
+	const ch = canvas.height;
+
+	flash++
+
+	if (textLoc >= 16) {
+		if (flash > 30 && flash < 60) {
+			context.globalAlpha = 1;
+			context.font = "32px Courier";
+			context.fillStyle = "#FFFFFF";
+			context.textAlign = "center";
+			context.fillText("Press Start To Play", cw/2, (ch/4) * 3);
+		}
+		else if(flash > 60){
+			flash = 0
+		}
+	}
+}
+
+export {menuBackground, menuBar, menuText, startText}
