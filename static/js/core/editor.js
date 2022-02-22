505e18a33d13b42ba98b385746b1966ece937980
Lucas Himschoot <34957838+WatermelonArray@users.noreply.github.com>
2022-02-22 14:13:15 +0000

    Added export checking for invalid custom quizes

--- static/js/core/editor.js
+++ static/js/core/editor.js
@@ -55,19 +55,43 @@ const changeValue = (callback, index) => {
 
 const exportJSON = (callback) => {
 
+	let allowed = true;
+	let reason = "";
 	let quiz = {"quizName": callback.editor.quizName};
 	let tempObj = {};
+	
+	console.log(callback.editor.questionList)
+	for (let i = 0; i < callback.editor.questionList.length; i++) {
+		if (callback.editor.questionList[i].answers.length === 0) {
+			allowed = false;
+			reason = "Some questions blank answers!";
+			break;
+		}
+		else {
+			let hasAnswer = false;
+			for (let x = 0; x < callback.editor.questionList[i].answers.length; x++) {
+				if (callback.editor.questionList[i].answers[x].isAnswer && !hasAnswer) {hasAnswer = true}
+			}
+			if (!hasAnswer) {
+					allowed = false;
+					reason = "Some questions have no correct answer set!";
+					break;
+				}
+			else {tempObj[i + 1] = callback.editor.questionList[i];}
+		}
+	}
+	if (allowed) {
+		quiz.questions = tempObj;
 
-	for (let i = 0; i < callback.editor.questionList.length; i++) {tempObj[i + 1] = callback.editor.questionList[i];}
-	quiz.questions = tempObj;
-
-	const base = btoa(JSON.stringify(quiz));
-	navigator.clipboard.writeText(base);
+		const base = btoa(JSON.stringify(quiz));
+		navigator.clipboard.writeText(base);
+	}
+	else {console.log(reason)}
 };
 
 const importJSON = (callback, id) => {
 
-	const tempid = "eyJxdWl6TmFtZSI6InF1aXpOYW1lIiwicXVlc3Rpb25zIjp7IjEiOnsicXVlc3Rpb24iOiJIZWxsbyBXb3JsZCIsImFuc3dlcnMiOlt7ImRlc2NyaXB0aW9uIjoiQSIsImlzQW5zd2VyIjpmYWxzZX0seyJkZXNjcmlwdGlvbiI6IkEiLCJpc0Fuc3dlciI6ZmFsc2V9LHsiZGVzY3JpcHRpb24iOiJBIiwiaXNBbnN3ZXIiOmZhbHNlfV19LCIyIjp7InF1ZXN0aW9uIjoiSGVsbG8gV29ybGQiLCJhbnN3ZXJzIjpbeyJkZXNjcmlwdGlvbiI6IkEiLCJpc0Fuc3dlciI6ZmFsc2V9LHsiZGVzY3JpcHRpb24iOiJBIiwiaXNBbnN3ZXIiOmZhbHNlfV19LCIzIjp7InF1ZXN0aW9uIjoiSGVsbG8gV29ybGQiLCJhbnN3ZXJzIjpbeyJkZXNjcmlwdGlvbiI6IkEiLCJpc0Fuc3dlciI6ZmFsc2V9LHsiZGVzY3JpcHRpb24iOiJBIiwiaXNBbnN3ZXIiOmZhbHNlfSx7ImRlc2NyaXB0aW9uIjoiQSIsImlzQW5zd2VyIjpmYWxzZX0seyJkZXNjcmlwdGlvbiI6IkEiLCJpc0Fuc3dlciI6ZmFsc2V9XX19fQ==";
+	const tempid = "eyJxdWl6TmFtZSI6IkVkaXRvciBUZXN0IFF1aXoiLCJxdWVzdGlvbnMiOnsiMSI6eyJxdWVzdGlvbiI6IlF1ZXN0aW9uIDEiLCJhbnN3ZXJzIjpbeyJkZXNjcmlwdGlvbiI6IkEiLCJpc0Fuc3dlciI6dHJ1ZX0seyJkZXNjcmlwdGlvbiI6IkIiLCJpc0Fuc3dlciI6ZmFsc2V9LHsiZGVzY3JpcHRpb24iOiJDIiwiaXNBbnN3ZXIiOmZhbHNlfV19LCIyIjp7InF1ZXN0aW9uIjoiUXVlc3Rpb24gMiIsImFuc3dlcnMiOlt7ImRlc2NyaXB0aW9uIjoiQSIsImlzQW5zd2VyIjpmYWxzZX0seyJkZXNjcmlwdGlvbiI6IkIiLCJpc0Fuc3dlciI6dHJ1ZX1dfSwiMyI6eyJxdWVzdGlvbiI6IlF1ZXN0aW9uIDMiLCJhbnN3ZXJzIjpbeyJkZXNjcmlwdGlvbiI6IkEiLCJpc0Fuc3dlciI6ZmFsc2V9LHsiZGVzY3JpcHRpb24iOiJCIiwiaXNBbnN3ZXIiOmZhbHNlfSx7ImRlc2NyaXB0aW9uIjoiQyIsImlzQW5zd2VyIjp0cnVlfSx7ImRlc2NyaXB0aW9uIjoiRCIsImlzQW5zd2VyIjpmYWxzZX1dfX19";
 	const temp = JSON.parse(atob(tempid));
 
 	callback.editor.currentQuestion = 1;
